9f3cc6689f240039ee74a578b4c035720521b5be
Sébastien Gahat <sebastien.gahat@MacBook-Pro-de-Sebastien.local>
2019-04-23 18:35:11 +0200

    cloud endpoint helloWorld

---
+++ test/app.test.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const path = require('path');
+const proxyquire = require('proxyquire').noCallThru();
+const request = require('supertest');
+const sinon = require('sinon');
+const assert = require('assert');
+const tools = require('@google-cloud/nodejs-repo-tools');
+
+const SAMPLE_PATH = path.join(__dirname, '../app.js');
+
+function getSample() {
+  const testApp = express();
+  const expressMock = sinon.stub().returns(testApp);
+  const app = proxyquire(SAMPLE_PATH, {
+    express: expressMock,
+  });
+
+  return {
+    app: app,
+    mocks: {
+      express: expressMock,
+    },
+  };
+}
+
+beforeEach(tools.stubConsole);
+afterEach(tools.restoreConsole);
+
+it(`sets up the sample`, done => {
+  const sample = getSample();
+
+  assert.ok(sample.mocks.express.calledOnce);
+  done();
+});
+
+it(`should display a helloworld message`, async () => {
+    const sample = getSample();
+    await request(sample.app)
+      .get('/hello')
+      .expect(200)
+      .expect(response => {
+        assert.strictEqual(response.body.message, 'HelloWorld!');
+      });
+  });
