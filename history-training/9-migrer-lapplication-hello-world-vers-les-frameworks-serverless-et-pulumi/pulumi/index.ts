2d742abf555ac04518867dc2de4b3998a5dedf8c
Sébastien Gahat <sgahat@octo.com>
2019-05-07 16:07:56 +0200

    add first pulumi backend

---
+++ history-training/9-migrer-lapplication-hello-world-vers-les-frameworks-serverless-et-pulumi/pulumi/index.ts
@@ -0,0 +1,9 @@
+import * as gcp from "@pulumi/gcp";
+
+let greeting = new gcp.cloudfunctions.HttpCallbackFunction("helloGet", (req, res) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.send("Hello World!");
+});
+
+export let url = greeting.httpsTriggerUrl;
