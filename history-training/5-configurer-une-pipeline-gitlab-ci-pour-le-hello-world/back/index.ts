b4cef5bc6e96339e41917ccc5e71c38e760e08a0
Sébastien Gahat <sgahat@octo.com>
2019-05-20 10:20:10 +0200

    init back gitlab ci

--- history-training/5-configurer-une-pipeline-gitlab-ci-pour-le-hello-world/back/index.ts
+++
@@ -1,9 +0,0 @@
-import * as gcp from "@pulumi/gcp";
-
-let greeting = new gcp.cloudfunctions.HttpCallbackFunction("helloPulumi", (req, res) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.send("Hello World!");
-});
-
-export let url = greeting.httpsTriggerUrl;
