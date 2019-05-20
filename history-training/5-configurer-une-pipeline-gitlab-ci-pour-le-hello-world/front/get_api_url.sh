aab54c138e80bea33e9f54f8a2d50df96554f4a3
Sébastien Gahat <sgahat@octo.com>
2019-05-20 10:48:03 +0200

    fix path for sh script

--- history-training/5-configurer-une-pipeline-gitlab-ci-pour-le-hello-world/front/get_api_url.sh
+++ history-training/5-configurer-une-pipeline-gitlab-ci-pour-le-hello-world/front/get_api_url.sh
@@ -4,12 +4,12 @@ REGION_ID=$(gcloud config get-value functions/region)
 
 ## Dev URL Setup
 echo "VUE_APP_TITLE=My App (GitlabCI)" > .env
-echo "VUE_APP_API_URL = $(cd ../back && pulumi stack output url)" >> .env
+echo "VUE_APP_API_URL = https://us-central1-stage-bof-search.cloudfunctions.net/helloGet" >> .env
 echo ".env is successfully setup, looks like:"
 cat .env
 
 ## Prod URL Setup
 echo "VUE_APP_TITLE=My App (GitlabCI)" > .env.production
-echo "VUE_APP_API_URL = $(cd ../back && pulumi stack output url)" >> .env.production
+echo "VUE_APP_API_URL = https://us-central1-stage-bof-search.cloudfunctions.net/helloGet" >> .env.production
 echo ".env.production is successfully setup, looks like:"
 cat .env.production
