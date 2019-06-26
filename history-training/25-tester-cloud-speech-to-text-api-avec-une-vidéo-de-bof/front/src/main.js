b36c95fe95226ac06ee993b7f22c970d113e7528
Sébastien Gahat <sgahat@octo.com>
2019-06-26 10:51:45 +0200

    front deletion for speech

--- history-training/25-tester-cloud-speech-to-text-api-avec-une-vidéo-de-bof/front/src/main.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue'
-import App from './App.vue'
-import 'vue2-dropzone/dist/vue2Dropzone.min.css'
-
-Vue.config.productionTip = false
-
-new Vue({
-  render: h => h(App)
-}).$mount('#app')
