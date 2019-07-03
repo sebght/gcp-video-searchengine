d7e10659a99b38a56920e1a6c25193f292b043cf
Sébastien Gahat <sgahat@octo.com>
2019-07-03 16:11:15 +0200

    first try back + front on pulumi/gitlabci

--- bof-search-app/back/index.js
+++
@@ -1,5 +0,0 @@
-exports.helloGet = (req,res) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.send("Hello World!");
-};
---
+++ bof-search-app/front/index.ts
@@ -0,0 +1,74 @@
+import * as pulumi from "@pulumi/pulumi";
+import * as gcp from "@pulumi/gcp";
+
+import * as fs from "fs";
+import * as mime from "mime";
+import * as path from "path";
+
+// Load the Pulumi program configuration. These act as the "parameters" to the Pulumi program,
+// so that different Pulumi Stacks can be brought up using the same code.
+
+const stackConfig = new pulumi.Config("static-website");
+const config = {
+  // pathToWebsiteContents is a relativepath to the website's contents.
+  pathToWebsiteContents: stackConfig.require("pathToWebsiteContents"),
+  bucketName: stackConfig.require("bucketName"),
+  region: stackConfig.require("region")
+};
+
+// contentBucket is the S3 bucket that the website's contents will be stored in.
+// TODO find a way to point to the correct name with the suffix
+const siteBucket = new gcp.storage.Bucket(config.bucketName, {
+  name: config.bucketName,
+  location: config.region,
+  websites: [
+    {
+      mainPageSuffix: "index.html",
+      notFoundPage: "404.html"
+    }
+  ]
+});
+
+// TODO replace 'Lecteur des anciens ensembles' par 'Lecteur normal'
+const defaultAcl = new gcp.storage.BucketACL("pulumi-demo-acl", {
+  bucket: siteBucket.name,
+  predefinedAcl: 'publicRead',
+});
+
+// crawlDirectory recursive crawls the provided directory, applying the provided function
+// to every file it contains. Doesn't handle cycles from symlinks.
+function crawlDirectory(dir: string, f: (_: string) => void) {
+  const files = fs.readdirSync(dir);
+  for (const file of files) {
+    const filePath = `${dir}/${file}`;
+    const stat = fs.statSync(filePath);
+    if (stat.isDirectory()) {
+      crawlDirectory(filePath, f);
+    }
+    if (stat.isFile()) {
+      f(filePath);
+    }
+  }
+}
+
+// Sync the contents of the source directory with the S3 bucket, which will in-turn show up on the CDN.
+const webContentsRootPath = path.join(
+  process.cwd(),
+  config.pathToWebsiteContents
+);
+console.log("Syncing contents from local disk at", webContentsRootPath);
+crawlDirectory(webContentsRootPath, (filePath: string) => {
+  const relativeFilePath = filePath.replace(webContentsRootPath + "/", "");
+  const contentFile = new gcp.storage.BucketObject(
+    relativeFilePath,
+    {
+      bucket: siteBucket.name,
+      contentType: mime.getType(filePath) || undefined,
+      name: relativeFilePath,
+      source: filePath,
+    },
+    {
+      parent: siteBucket
+    }
+  );
+});
