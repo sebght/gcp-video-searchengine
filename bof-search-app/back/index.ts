fbe6fccfbffded4f08956ebab080be43e5f402f3
Sébastien Gahat <sgahat@octo.com>
2019-07-27 21:45:00 +0200

    added signed url function

--- bof-search-app/back/index.ts
+++ bof-search-app/back/index.ts
@@ -1,14 +1,17 @@
 import * as gcp from "@pulumi/gcp";
 import { asset } from "@pulumi/pulumi";
 
-const functionName = "helloGet"
-const bucketName = functionName.toLowerCase()
+const functionhwName = "helloGet"
+const functionsuName = "getSignedURL_bof"
+const bucketName = "sega"
 const regionName = "us-central1"
 
 const bucket = new gcp.storage.Bucket(`${bucketName}_bofsearch`,{
   name: `${bucketName}_bofsearch`,
   location: regionName
 });
+
+// Hello World
 
 const bucketObjecthw = new gcp.storage.BucketObject("hw-zip", {
   bucket: bucket.name,
@@ -17,13 +20,32 @@ const bucketObjecthw = new gcp.storage.BucketObject("hw-zip", {
   }),
 });
 
-const functionhw = new gcp.cloudfunctions.Function(functionName, {
+const functionhw = new gcp.cloudfunctions.Function(functionhwName, {
   sourceArchiveBucket: bucket.name,
   runtime: "nodejs10",
   sourceArchiveObject: bucketObjecthw.name,
   entryPoint: "helloGet",
   triggerHttp: true,
-  name: functionName
+  name: functionhwName
+});
+
+// Fonction qui renvoit une Signed URL au client
+
+const bucketObjectSignedURL = new gcp.storage.BucketObject("su-zip", {
+  bucket: bucket.name,
+  source: new asset.AssetArchive({
+      ".": new asset.FileArchive("./signed-url"),
+  }),
+});
+
+const functionSignedURL = new gcp.cloudfunctions.Function(functionsuName, {
+  sourceArchiveBucket: bucket.name,
+  runtime: "nodejs10",
+  sourceArchiveObject: bucketObjectSignedURL.name,
+  entryPoint: "getSignedUrl",
+  triggerHttp: true,
+  name: functionsuName
 });
 
 export let helloGetEndpoint = functionhw.httpsTriggerUrl;
+export let SignedPostEndpoint = functionSignedURL.httpsTriggerUrl;
