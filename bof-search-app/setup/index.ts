96b8d87aa78c0efa0449178b0cd0d10d398107da
Sébastien Gahat <sgahat@octo.com>
2019-07-29 18:25:29 +0200

    init pulumi setup source bucket

---
+++ bof-search-app/setup/index.ts
@@ -0,0 +1,27 @@
+import * as pulumi from "@pulumi/pulumi";
+import * as gcp from "@pulumi/gcp";
+
+const stackConfig = new pulumi.Config("setup");
+const config = {
+  sourceBucketName: stackConfig.require("sourceBucketName"),
+  region: stackConfig.require("region")
+};
+
+const cors_source = [
+    {
+      origins: ["*"],
+      responseHeaders: ["Content-Type", "Authorization",  "Content-Length", "User-Agent", "x-goog-resumable"],
+      methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+      maxAgeSeconds: 3600
+    }
+]
+
+// Create a GCP resource (Storage Bucket)
+const bucket = new gcp.storage.Bucket(config.sourceBucketName, {
+    name: config.sourceBucketName,
+    location: config.region,
+    cors: cors_source
+});
+
+// Export the DNS name of the bucket
+export const bucketName = bucket.url;
