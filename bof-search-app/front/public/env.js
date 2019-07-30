68241e8d579851d6549b6f3c0b5143b058575b1e
Sébastien Gahat <sgahat@octo.com>
2019-07-30 23:27:06 +0200

    config BoRe for the win

---
+++ bof-search-app/front/public/env.js
@@ -0,0 +1,6 @@
+window.env = {
+    SIGN_URL: 'https://us-central1-bof-search.cloudfunctions.net/getSignedURL',
+    FIRESTORE_URL: 'https://us-central1-bof-search.cloudfunctions.net/updateFirestore',
+    VIDEO_BUCKET: 'audio-source-bof'
+  }
+    
