61ac1a6ba9df5265b1b337e3c96a0ef5ff3aa750
Sébastien Gahat <sgahat@octo.com>
2019-08-02 14:40:50 +0200

    added todo remove env.js from public folder

--- bof-search-app/client/public/env.js
+++ bof-search-app/client/public/env.js
@@ -1,6 +1,6 @@
+// TODO remove this file, as it will be injected after build
 window.env = {
-    SIGN_URL: 'https://us-central1-bof-search.cloudfunctions.net/getSignedURL',
-    FIRESTORE_URL: 'https://us-central1-bof-search.cloudfunctions.net/createBof',
-    VIDEO_BUCKET: 'audio-source-bof'
-  }
-    
+  SIGN_URL: "https://us-central1-bof-search.cloudfunctions.net/getSignedURL",
+  FIRESTORE_URL: "https://us-central1-bof-search.cloudfunctions.net/createBof",
+  VIDEO_BUCKET: "audio-source-bof"
+};
