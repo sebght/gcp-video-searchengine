df7488c877e2520d19f5103f501255bfec4e69c3
Sébastien Gahat <sgahat@octo.com>
2019-08-05 14:07:00 +0200

    removed env.js from client/public

--- bof-search-app/client/public/env.js
+++
@@ -1,6 +0,0 @@
-// TODO remove this file, as it will be injected after build
-window.env = {
-  SIGN_URL: "https://us-central1-bof-search.cloudfunctions.net/getSignedURL",
-  FIRESTORE_URL: "https://us-central1-bof-search.cloudfunctions.net/createBof",
-  VIDEO_BUCKET: "audio-source-bof"
-};
