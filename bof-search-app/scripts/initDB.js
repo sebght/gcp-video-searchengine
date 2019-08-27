ac9c359e074107a4aa989099b0f86ff80bc93cf8
Sébastien Gahat <sgahat@octo.com>
2019-08-27 14:43:15 +0200

    first script for initializing Firestore

---
+++ bof-search-app/scripts/initDB.js
@@ -0,0 +1,16 @@
+const Firestore = require('@google-cloud/firestore');
+const db = new Firestore();
+const content = require("./bofSamples.json");
+
+for(const bofKey in content) {
+    console.log(bofKey+": "+content[bofKey].name);
+    let docRef = db.collection("bofs").doc();
+    let setDoc = docRef.set({
+        name: content[bofKey].name,
+        description: content[bofKey].description,
+        speaker: content[bofKey].speaker,
+        thumbnailUrl: content[bofKey].thumbnailUrl,
+        videoUrl: content[bofKey].videoUrl,
+        tags: content[bofKey].tags
+    });
+}
